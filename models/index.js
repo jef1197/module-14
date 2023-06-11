cdfb324b0a28fc4fd3fc592c091b725380c03c34
JefO <jef.ogu@gmail.com>
2023-06-10 22:49:23 -0400

    cleaning up index
    cleaning up

--- models/index.js
+++ models/index.js
@@ -5,27 +5,27 @@ const Comment = require('./Comment');
 User.hasMany(Post, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
-})
+});
 
 Post.belongsTo(User, {
   foreignKey: 'user_id'
-})
+});
 
 User.hasMany(Comment, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
-})
-
-Post.hasMany(Comment, {
-  foreignKey: 'post_id',
-})
-
-Comment.belongsTo(Post, {
-  foreignKey: 'post_id',
-})
+});
 
 Comment.belongsTo(User, {
   foreignKey: 'user_id',
-})
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+});
 
 module.exports = { User, Post, Comment };
