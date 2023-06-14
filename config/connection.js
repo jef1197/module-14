08d3f4bf0b6f4d625b9edb0eee7bba09249bd377
JefO <jef.ogu@gmail.com>
2023-06-13 20:59:41 -0400

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -12,7 +12,7 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_PASSWORD,
     {
       host: process.env.DB_HOST,
-      dialect: 'mysql',
+      dialect: 'postgres',
       port: process.env.DB_PORT
     }
   );
