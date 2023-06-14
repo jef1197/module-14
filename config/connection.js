cfa2e3ad23fa150250893b31ad74bea5d664aeff
JefO <jef.ogu@gmail.com>
2023-06-13 20:50:02 -0400

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -6,25 +6,16 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(process.env.DB_NAME, {
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: 'postgres',
+      port: process.env.DB_PORT
     }
-  }
-);
-
-  // sequelize = new Sequelize(
-  //   process.env.DB_NAME,
-  //   process.env.DB_USER,
-  //   process.env.DB_PASSWORD,
-  //   {
-  //     dialect: 'mysql',
-  //     port: 3306
-  //   }
-  // );
+  );
 }
 
 module.exports = sequelize;
