dec6bf86b0ef2e4add9df5a0055270e838a71003
RoelleMarie <marie9n@gmail.com>
2023-06-19 21:05:07 -0400

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,31 +1,22 @@
-// const Sequelize = require('sequelize');
-// require('dotenv').config();
-
-// let sequelize;
-
-// if (process.env.JAWSDB_URL) {
-//   sequelize = new Sequelize(process.env.JAWSDB_URL);
-// } else {
-//   sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PASSWORD,
-//     {
-//       host: process.env.DB_HOST,
-//       dialect: 'postgres',
-//       port: process.env.DB_PORT
-//     }
-//   );
-// }
-
-const { Sequelize } = require('sequelize')
+const Sequelize = require('sequelize');
 require('dotenv').config();
-const { DB_HOST, DB_NAME, DB_USER, DB_PASSWORD } = process.env
 
-let sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  host: DB_HOST,
-  dialect: 'postgres',
-  logging: false,
-});
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    },
+  );
+}
+
 
 module.exports = sequelize;
